ed20d84a7297469b07daa991cf5b1a675947851c
Bangdik11 <indandika0@gmail.com>
2021-08-14 03:11:36 +0800

    Update listreport.php

--- listreport.php
+++ listreport.php
@@ -16,7 +16,7 @@
              while ($bariss = mysqli_fetch_assoc($queryKomentar) ) {
                 
                 // if($queryKomentar->num_rows>0){
-                     echo $bariss;
+                    //  echo $bariss;
                     $baris = array_merge($baris,array("lihat"=>$bariss["lihat"]));
                 // }else{
                 //     $baris = array_merge($baris,array("lihat"=>1));
